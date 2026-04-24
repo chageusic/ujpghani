728ad5c7a91a812848061a25696a23d570086710
chageusic <chageusic@gmail.com>
2026-04-24 23:28:25 +0900

    feat: Add Link component for navigation
    Introduces the `Link` component from `react-router-dom` to enable declarative navigation within the application. This is a foundational step for building interactive navigation elements.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation, Link } from 'react-router-dom';
 import { Header } from './components/Header';
 import { FloatingMenu } from './components/FloatingMenu';
 import { Home } from './pages/Home';
